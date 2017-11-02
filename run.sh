73412eb7ec69458c05cf6b76b34d92e041e24aa4
daisuke_s88 <sinmachi@soka.ac.jp>
2017-11-02 17:00:10 +0900

    modified dir path

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run -d -v ~/workdir/glycoprotein/sparqlist/repository:/opt/git/sparqlist/repository -p 8089:3000 sparqlist:v1.0
+docker run -d -v ~/workdir/sparqlist/repository:/opt/git/sparqlist/repository -p 8089:3000 sparqlist:v1.0
